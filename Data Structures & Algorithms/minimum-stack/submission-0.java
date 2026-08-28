84dafcf63b3cfe5ebc749f7d70bab97c47942b7a
Shivang Parmar <234321566+shivangparmar91-lgtm@users.noreply.github.com>
2026-08-28 19:55:03 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,35 @@
+class MinStack {
+
+    Deque<Integer> stack = new ArrayDeque<>();
+    Deque<Integer> minstack = new ArrayDeque<>();
+
+    public MinStack() {
+        
+    }
+    
+    public void push(int val) {
+        stack.push(val);
+
+        if(minstack.isEmpty())
+        {
+            minstack.push(val);
+        }
+        else 
+        {
+            minstack.push(Math.min(val,minstack.peek()));
+        }
+    }
+    
+    public void pop() {
+        stack.pop();
+        minstack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minstack.peek();
+    }
+}
